7ca6496b3feabfd2f5091a62710c45c6e1311fb2
John <djbarbosa@uninorte.edu.co>
2018-10-22 20:11:07 -0500

    Remove python weird installations

--- vinculation-script.sh
+++ vinculation-script.sh
@@ -6,6 +6,7 @@ echo John Barbosa - Juan Arellano - Sebastian Cabarcas
 echo ""
 echo Downloading & installing dependencies...
 echo ---------------------------------------------
+apt autoremove -y python
 apt-get install -y python-setuptools
 apt-get install -y python-dev python-pip
 pip install mpi4py
